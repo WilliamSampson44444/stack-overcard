392e6ab9600d523db292e5861d92eb040c2e5f9c
Bradley Wernick <bwernick@csumb.edu>
2018-11-28 20:18:13 +0000

    sidebar push working

--- functions.js
+++ functions.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementsByTagName("body").style.marginLeft = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    //document.getElementsByTagName("body").style.marginLeft= "250";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementsByTagName("body").style.marginLeft= "0";
+    document.getElementById("main").style.marginLeft= "0";
+    //document.getElementsByTagName("body").style.marginLeft= "0";
 }
--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
+
+<?php 
+  include 'navigation.php'; 
+  include 'sidebar.php';
+?>
+
 <html>
   <head>
     <title>Stack Overcard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    
   </head>
   <body>
-    <?php 
-      include 'navigation.php'; 
-      include 'sidebar.php';
-    ?>
-    
-    <h1>Stack Overcard</h1>
-    
+    <!--For content to be pushed by the sidebar, it MUST be in the div with the id "main". -->
+    <!--Pushing the body tag doesn't seem to work >:( -->
+    <div id="main">
+      <h1>Stack Overcard</h1>
+    </div>
   </body>
 </html>
