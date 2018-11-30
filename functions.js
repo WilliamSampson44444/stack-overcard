07c91ee3db9d0fec9456255fb9e4a8492552a66c
Bradley Wernick <bwernick@csumb.edu>
2018-11-30 20:42:36 +0000

    functions cleanup

--- functions.js
+++ functions.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
- 
-script.src="https://code.jquery.com/jquery-3.3.1.min.js";
-document.getElementsByTagName('head')[0].appendChild(script); 
-
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -14,24 +9,3 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
     //document.getElementsByTagName("body").style.marginLeft= "0";
 }
-
-$("#decks").change(function(){
-    $.ajax({
-
-        type: "GET",
-        url: "getDecks.php",
-        dataType: "json",
-        data: { "username": $("#username").val() },
-        success: function(data,status) {
-        
-            alert(data);
-            
-            //Do things - populate sidebar?
-        
-        },
-        complete: function(data,status) { //optional, used for debugging purposes
-            alert(status);
-        }
-        
-    });
-});
