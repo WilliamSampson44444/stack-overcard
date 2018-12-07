917bd88ede49aad8e2669f2a4f02d57cdd86e877
William Sampson <wsampson@csumb.edu>
2018-12-07 21:12:38 +0000

    Messing with modals

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
       </button>
       
       <!-- Modal -->
-      <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+      <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="false">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
