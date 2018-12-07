3507072736aff5a221c31f5f09eb1e1b3d9b7c9a
Bradley Wernick <bwernick@csumb.edu>
2018-12-07 21:42:44 +0000

    formatting fixes for modals

--- getCards.php
+++ getCards.php
@@ -14,7 +14,7 @@
     echo '<div class="flex-wrapper">';
     foreach ($records as $record){
         //var_dump($record);
-        echo '<button type="button" class="cards btn btn-primary" data-toggle="modal" data-target="#exampleModalLong" id ="' . $record['card_id'] . '" onclick="getAnswers(this.id);">' . $record['question'] . '</button>';
+        echo '<button type="button" class="cards btn btn-primary" data-toggle="modal" data-target="#exampleModalLong" id ="' . $record['card_id'] . '" onclick="getAnswers(this.id);"><div class="cardText">' . $record['question'] . '</div></button>';
     }
     echo '</div>';
 ?>
