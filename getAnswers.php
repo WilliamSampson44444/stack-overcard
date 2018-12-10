0fe7ca12fd4bf62a5cd5710d21e961aaf70b2298
Bradley Wernick <bwernick@csumb.edu>
2018-12-10 20:28:05 +0000

    added arrows

--- getAnswers.php
+++ getAnswers.php
@@ -17,8 +17,10 @@
         echo '<div class="flex-wrapper" id="innerFlex">';
             echo '<div class="flex-wrapper" id="scoreFlex">';
                 //put upvote div here
+                echo '<div class="upvote" onclick="upvote();"><i class="fa fa-arrow-up" style="font-size:36px;color:grey;"></i></div>';
                 echo '<div class="answers" id ="score' . $record['answer_id'] . '"> Score: ' . $record['rating'] . '</div>';
                 //put downvote div here
+                echo '<div class="downvote" onclick="downvote();"><i class="fa fa-arrow-down" style="font-size:36px;color:grey;"></i></div>';
             echo '</div>';
             echo '<div id="answer' . $record['answer_id'] . '">' . $record['answer'] . '</div>';
         echo '</div>';
--- profile.php
+++ profile.php
@@ -18,6 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>   
     <script
