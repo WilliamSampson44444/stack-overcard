ce25ffdf7e902a5002d00fc0ea434131f91fd27e
William Sampson <wsampson@csumb.edu>
2018-12-10 21:48:10 +0000

    cleanup

--- navigation.php
+++ navigation.php
@@ -1,18 +1,3 @@
-<script>
-window.onscroll = function() {stickyBar()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function stickyBar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-</script>
-
 <nav class="navbar navbar-default" role="navigation">
   <div class="navbar-header">
     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
